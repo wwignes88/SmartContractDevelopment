b1c56ee21eaa28bf60683af726311462a84e14fd
Wayne Wignes <101439411+wwignes88@users.noreply.github.com>
2023-08-26 08:40:58 -0700

    Add files via upload

--- createNFT/contracts/Collectible.sol
+++ createNFT/contracts/Collectible.sol
@@ -11,6 +11,7 @@ import "node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URISt
 import "node_modules/@openzeppelin/contracts/utils/Strings.sol";
 
 
+
 contract NFT is ERC721URIStorage{
     LinkTokenInterface internal immutable LINK;
     using Strings for uint256;
@@ -20,4 +21,52 @@ contract NFT is ERC721URIStorage{
 	  mapping(uint256 => address) TokenOwners;
     event requestedCollectible(uint256 TokenId_, address requester);
 
-  
+
+    constructor(address _linkToken) public 
+    ERC721("Stright Jacket", "STR8")
+    {
+        tokenCounter = 0;
+        LINK = LinkTokenInterface(_linkToken);
+    }
+
+//======================================================================
+//====================== MY VRFConsumerBase funcs ======================
+
+    receive() external payable {}
+    fallback() external payable {}
+    //function deposit() public payable {}
+
+    function LINKBalance(address QueryAddress) public view returns (uint256 Balance_){
+      Balance_ = LINK.balanceOf(QueryAddress);
+    }
+
+//======================================================================
+//======================  NFT FUNCS   ==================================
+
+
+	function createCollectible( address owner, string memory _tokenURI) public  {
+        // token Id
+        uint256 newTokenId = tokenCounter;
+		    TokenOwners[newTokenId] = owner;
+        _safeMint(owner, newTokenId);
+        _setTokenURI( newTokenId, _tokenURI);
+        tokenCounter = newTokenId + 1;
+        requestedCollectible(newTokenId, owner);
+    }
+  function getTokenOwner(uint256 tokenId) public view returns (address){
+    return TokenOwners[tokenId];
+  }
+
+
+
+// should have onlyOwner modifier!!
+  function ChangeTokenURI(uint256 tokenId, string memory _tokenURI) public {
+      _setTokenURI( tokenId, _tokenURI);
+  }
+
+
+  function GetTokenURI(uint256 tokenId) public view returns(string memory){
+      return tokenURI( tokenId);
+  }
+
+}
